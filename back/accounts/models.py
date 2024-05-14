466681a7ff7b85d195ac2e7661a8046984c943af
복현우 <bhwoo1001@naver.com>
2024-05-14 23:57:33 +0900

    signoutfailed

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -3,10 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    userid = models.CharField(max_length=50, unique=True)  # id
     name = models.CharField(max_length=50)  # 이름(or 별명)
     email = models.EmailField(max_length=250, blank=True, null=True) # email
-    date_of_birth = models.DateField(verbose_name = '생년월일')
+    date_of_birth = models.DateField(verbose_name = '생년월일', blank=True, null=True)
     profile_img = models.ImageField(upload_to='image/', default='image/userdefault.png')
     budget = models.IntegerField(blank=True, null=True)  # 보유 자산
     salary = models.IntegerField(blank=True, null=True) # 월 수익
@@ -18,4 +17,3 @@ class User(AbstractUser):
     saving_period = models.IntegerField(blank=True, null=True) # 적금 예치 기간
     is_superuser = models.BooleanField(default=False) # 관리자여부
     
-    USERNAME_FIELD = 'userid'
