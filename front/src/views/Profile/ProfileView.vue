3b06857ee043fba396cabc70b2ff8a28fde2f8d5
복현우 <bhwoo1001@naver.com>
2024-05-15 21:32:33 +0900

    profile get, put

---
+++ front/src/views/Profile/ProfileView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>ProfileView</h1>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<style scoped></style>
