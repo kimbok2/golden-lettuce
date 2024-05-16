ae0bb7294ff1f2b053a269c3343a8aa7a1f6eb92
복현우 <bhwoo1001@naver.com>
2024-05-16 11:48:15 +0900

    index.js 수정

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -120,10 +120,7 @@ const freeAccessNames = [
 ];
 
 // 로그인시 접근 불가능한 name 정의
-const loginBlockAccessNames = [
-  'login',
-  'signup',
-]
+const loginBlockAccessNames = ["login", "signup"];
 
 router.beforeEach((to, from) => {
   const userStore = useUserStore();
@@ -134,12 +131,10 @@ router.beforeEach((to, from) => {
     alert("로그인이 필요합니다.");
     router.push({ name: "login" });
   }
-
-});
   if (loginBlockAccessNames.includes(to.name) && isLogin) {
-    alert('이미 로그인된 사용자입니다.')
-    next(false)
+    alert("이미 로그인된 사용자입니다.");
+    next(false);
   }
-})
+});
 
 export default router;
