9cba2ae35aef652fb59228260af3bf371730031d
myDdoDdomi <gumtteun@gmail.com>
2024-05-16 17:44:03 +0900

    update back / community related models

--- back/communities/urls.py
+++ back/communities/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # 게시글
     path('', views.article_list),
-    path('<int:article_pk>/', views.article_detail)
-    # path('<int:article_pk>/comment/', views.article_comment)
+    path('<int:article_pk>/', views.article_detail),
+    path('<int:article_pk>/comment/', views.article_comment),
+    path('comment/<int:comment_pk>/', views.comment_delete),
 ]
