ff9fd72abfa76d19f1592613289708a563a57fd0
myDdoDdomi <gumtteun@gmail.com>
2024-05-16 09:14:14 +0900

    update

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createRouter, createWebHistory } from 'vue-router'
 import CommunityView from '@/views/Community/CommunityView.vue'
 import CommunityCreateView from '@/views/Community/CommunityCreateView.vue'
@@ -9,110 +8,106 @@ import ExchangeView from '@/views/ExchangeView.vue'
 import HomeView from '@/views/HomeView.vue'
 import MapsView from '@/views/MapsView.vue'
 import MyAccountView from '@/views/MyAccountView.vue'
-import ProfileView from '@/views/ProfileView.vue'
+import ProfileView from '@/views/Profile/ProfileView.vue'
+import ProfileInfoView from '@/views/Profile/ProfileInfoView.vue'
+import ProfileUpdateView from '@/views/Profile/ProfileUpdateView.vue'
 import ProductCompareView from '@/views/ProductCompareView.vue'
 import ProductListView from '@/views/ProductListView.vue'
 import LoginView from '@/views/LoginView.vue'
 import SignUpView from '@/views/SignUpView.vue'
-=======
-import { createRouter, createWebHistory } from "vue-router";
-import CommunityView from "@/views/Community/CommunityView.vue";
-import CommunityCreateView from "@/views/Community/CommunityCreateView.vue";
-import CommunityDetailView from "@/views/Community/CommunityDetailView.vue";
-import CommunityHomeView from "@/views/Community/CommunityHomeView.vue";
-import ExchangeView from "@/views/ExchangeView.vue";
-import HomeView from "@/views/HomeView.vue";
-import MapsView from "@/views/MapsView.vue";
-import MyAccountView from "@/views/MyAccountView.vue";
-import ProfileView from "@/views/Profile/ProfileView.vue";
-import ProfileInfoView from "@/views/Profile/ProfileInfoView.vue";
-import ProfileUpdateView from "@/views/Profile/ProfileUpdateView.vue";
-import ProductCompareView from "@/views/ProductCompareView.vue";
-import ProductListView from "@/views/ProductListView.vue";
-import LoginView from "@/views/LoginView.vue";
-import SignUpView from "@/views/SignUpView.vue";
->>>>>>> c6285f064ecf31f67ce62a7de0f0ac721d832832
+
+import { useUserStore } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/compare/",
-      name: "compare",
+      path: '/compare/',
+      name: 'compare',
       component: ProductCompareView,
     },
     {
-      path: "/products/",
-      name: "products",
+      path: '/products/',
+      name: 'products',
       component: ProductListView,
     },
     {
-      path: "/community/",
+      path: '/community/',
       component: CommunityView,
       children: [
-<<<<<<< HEAD
         { path: '', name: 'community', component: CommunityHomeView },
         { path: 'create', name: 'community-create', component: CommunityCreateView },
         { path: ':id', name: 'community-detail', component: CommunityDetailView },
         { path: ':id/update', name: 'community-update', component: CommunityUpdateView },
-=======
-        { path: "", name: "community", component: CommunityHomeView },
-        {
-          path: "create",
-          name: "community-create",
-          component: CommunityCreateView,
-        },
-        {
-          path: ":id",
-          name: "community-detail",
-          component: CommunityDetailView,
-        },
->>>>>>> c6285f064ecf31f67ce62a7de0f0ac721d832832
       ],
     },
     {
-      path: "/maps/",
-      name: "maps",
+      path: '/maps/',
+      name: 'maps',
       component: MapsView,
     },
     {
-      path: "/user/",
-      name: "user",
+      path: '/user/',
+      name: 'user',
       component: MyAccountView,
     },
     {
-      path: "/exchange/",
-      name: "exchange",
+      path: '/exchange/',
+      name: 'exchange',
       component: ExchangeView,
     },
     {
-      path: "/profile/",
+      path: '/profile/',
       component: ProfileView,
       children: [
-        { path: "", name: "profile", component: ProfileInfoView },
+        { path: '', name: 'profile', component: ProfileInfoView },
         {
-          path: "update",
-          name: "profile-update",
+          path: 'update',
+          name: 'profile-update',
           component: ProfileUpdateView,
         },
       ],
     },
     {
-      path: "/login/",
-      name: "login",
+      path: '/login/',
+      name: 'login',
       component: LoginView,
     },
     {
-      path: "/signup/",
-      name: "signup",
+      path: '/signup/',
+      name: 'signup',
       component: SignUpView,
     },
   ],
-});
+})
 
-export default router;
+// 로그인 없이 접근 가능한 name 정의
+const freeAccessNames = [
+  'home',
+  'compare',
+  'products',
+  'community',
+  'community-detail',
+  'maps',
+  'exchange',
+  'login',
+  'signup',
+]
+
+router.beforeEach((to, from) => {
+  const userStore = useUserStore()
+  const isLogin = userStore.isLogin
+  // 비로그인 유저에 대해 로그인 필수 페이지 진입시 login alert 창을 띄워주고,
+  // 로그인 페이지로 redirect
+  if (!freeAccessNames.includes(to.name) && !isLogin) {
+    alert('로그인이 필요합니다.')
+    router.push({ name: 'login' })
+  }
+})
+
+export default router
